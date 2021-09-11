b6a05d9670b78610bbaa8a501577401f837bd20b
onedss <onedss@qq.com>
2021-09-11 17:06:05 +0800

    add config.

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"github.com/onedss/httpFileServer/utils"
+	"net/http"
+)
 
 func main() {
-	http.ListenAndServe(":8181", http.FileServer(http.Dir("e:/")))
+	rootPath := fmt.Sprintf("%s/www", utils.CWD())
+	httpPort := utils.Conf().Section("http").Key("port").MustInt(8181)
+	webroot := utils.Conf().Section("http").Key("webroot").MustString(rootPath)
+	address := fmt.Sprintf(":%d", httpPort)
+	fmt.Println("Starting Http File Server... ")
+	fmt.Println("Listen Port:", httpPort, "")
+	fmt.Println("ROOT PATH:", webroot, "")
+	http.ListenAndServe(address, http.FileServer(http.Dir(webroot)))
 }
